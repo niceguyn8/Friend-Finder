c2f38f65abf637a4a085182d84d0958907dcc17f
Nathan Schubert <schubert.nathan@gmail.com>
2018-05-14 16:54:43 -0400

    html and data js

---
+++ app/data/friends.js
@@ -0,0 +1,82 @@
+[
+{
+name: "Captain America",
+photo: "https://cdn.britannica.com/700x450/30/182830-004-61C7794A.jpg",
+scores: [
+"5",
+"1",
+"4",
+"4",
+"5",
+"1",
+"2",
+"5",
+"4",
+"1"
+]
+},
+{
+name: "Spider-Man",
+photo: "https://media.wired.com/photos/5963bb8f9a706f225866c776/master/w_1200,c_limit/Spiderman-TopArt.jpg",
+scores: [
+"4",
+"2",
+"5",
+"1",
+"3",
+"2",
+"2",
+"1",
+"3",
+"2"
+]
+},
+{
+name: "Black Panther",
+photo: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2018/04/11/105124014-Screen-Shot-2018-04-11-at-8.13.44-AM.530x298.jpg?v=1523448997",
+scores: [
+"5",
+"2",
+"2",
+"2",
+"4",
+"1",
+"3",
+"2",
+"5",
+"5"
+]
+},
+{
+name: "The Hulk",
+photo: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fdanidiplacido%2Ffiles%2F2017%2F10%2FhpNTUvvUfJCx.jpg",
+scores: [
+"3",
+"3",
+"4",
+"2",
+"2",
+"1",
+"3",
+"2",
+"2",
+"3"
+]
+},
+{
+name: "Black Widow",
+photo: "http://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/51ce6099e4b0d911b4489b79/52499edfe4b05a24380fe54f/1380564584656/joss-whedon-on-black-widow-and-james-spader-on-ultron-in-avengers-2-preview.jpg?format=1000w",
+scores: [
+"4",
+"3",
+"4",
+"1",
+"5",
+"2",
+"5",
+"3",
+"1",
+"4"
+]
+}
+]
